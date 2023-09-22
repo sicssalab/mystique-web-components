4f297775e755e31db677ddab57e7ec2bf98975f1
Leonidas Segura <suanhero@gmail.com>
2023-09-22 12:00:10 -0500

    eliminacion de stories sin uso

--- src/components/cards/MystiqueCardBootstrap.js
+++ src/components/cards/MystiqueCardBootstrap.js
@@ -1,5 +1,5 @@
+import React from "react";
 import { Button } from "react-bootstrap";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import Card from "react-bootstrap/Card";
 
 const MystiqueCardBootstrap = (props) => {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Autocomplete from "../src/components/autocompletes/Autocomplete";
+import DoubleAutocomplete from "../src/components/autocompletes/DoubleAutocomplete";
+import MystiqueCard from "../src/components/cards/MystiqueCard";
+import MystiqueCardBootstrap from "../src/components/cards/MystiqueCardBootstrap";
+import MystiqueTallCard from "../src/components/cards/MystiqueTallCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export {
+  Autocomplete,
+  DoubleAutocomplete,
+  MystiqueCard,
+  MystiqueCardBootstrap,
+  MystiqueTallCard,
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
